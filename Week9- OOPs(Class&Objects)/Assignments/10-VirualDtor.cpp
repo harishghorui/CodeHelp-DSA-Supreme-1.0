96eab17eb3371ea0ea6626ecad4b1b460b5a6ef2
harishghorui <harishghorui2@gmail.com>
2023-09-09 18:03:21 +0530

    Always make virtual Dtor while Inheritance

---
+++ Week9- OOPs(Class&Objects)/Assignments/10-VirualDtor.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+    public:
+
+    Base() {
+        cout << "Base Ctor" << endl;
+    }
+
+    // ~Base() {
+    //     cout << "Base Dtor" << endl;
+    // }
+    // Without virtual Dtor, Derived Dtor is not called
+
+    virtual ~Base() {
+        cout << "Base Dtor" << endl;
+    }
+};
+
+class Derived : public Base {
+    public:
+    int *a;
+
+    Derived() {
+        a = new int[1000]; //without Virtual this may lead to memory leak
+        cout << "Derived Ctor" << endl;
+    }
+
+    ~Derived() {
+        cout << "Derived Dtor" << endl;
+    }
+};
+
+int main() {
+
+    Base *a = new Derived();
+    delete a;
+
+    return 0;
+}
